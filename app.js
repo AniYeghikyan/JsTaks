d080aa7979b8760a533d4bb29171676d779c7184
AniYeghikyan <ANIYEGHIKYAN94@mail.ru>
2019-06-01 19:00:53 +0400

    chenage

--- app.js
+++
@@ -1,7 +0,0 @@
-async function getUserAsync(name) {
-    let response = await fetch ('https://jsonplaceholder.typicode.com/users/');
-    let data = await response.json()
-    return data;
-}
-getUserAsync('id')
-    .then(data => console.log(data))
